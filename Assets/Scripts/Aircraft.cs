780f22cfce8f283e829ca9ec6d6a0e0ede78f583
RazorbreakRZ <razorbreak@gmail.com>
2018-05-04 00:02:04 +0200

    Added keyboard controllers. Fixed some bg problems. Added new scripts for camera and player.

---
+++ Assets/Scripts/Aircraft.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Aircraft : MonoBehaviour {
+
+    float engineForce = 10f;
+    float rotationForce = -100f;
+    float driftFactor = 1f;
+
+    // Use this for initialization
+    void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        Rigidbody2D rb = GetComponent<Rigidbody2D>();
+
+        rb.velocity = ForwardVelocity() + RightVelocity() * driftFactor;
+        if (Input.GetButton("Acceleration"))
+            rb.AddForce(transform.up * engineForce);
+        rb.angularVelocity = Input.GetAxis("Rotation") * rotationForce;
+    }
+
+    Vector2 ForwardVelocity()
+    {
+        return transform.up * Vector2.Dot(GetComponent<Rigidbody2D>().velocity, transform.up);
+    }
+
+    Vector2 RightVelocity()
+    {
+        return transform.right * Vector2.Dot(GetComponent<Rigidbody2D>().velocity, transform.right);
+    }
+}
--- Assets/Scripts/FollowUV.cs
+++ Assets/Scripts/FollowUV.cs
@@ -2,18 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ScrollUV : MonoBehaviour {
+public class FollowUV : MonoBehaviour {
 
     // Use this for initialization
-    //void Start () {}
+    void Start()
+    {
+
+    }
 
     // Update is called once per frame
-    void Update () {
+    void Update ()
+    {
 		MeshRenderer mr = GetComponent<MeshRenderer> ();
 		Material mat = mr.material;
 		Vector2 offset = mat.mainTextureOffset;
-		offset.x += Time.deltaTime / 10f;
-		offset.y += Time.deltaTime / 10f;
+        offset.x = transform.position.x / transform.localScale.x / parralax;
+		offset.y = transform.position.y / transform.localScale.y / parralax;
 		mat.mainTextureOffset = offset;
 	}
+
+    float parralax = 1f;
 }
